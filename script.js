4d513ad1d342c0178b671e75f2095b66a7106c39
kl25abc <kl25abc@herts.ac.uk>
2026-01-10 22:37:34 +0000

    Changed how info is sorted

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ async function fetchProjects() {
     ];
 
     try {
-        const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+        const response = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
         if (!response.ok) throw new Error("API Limit");
 
         const repos = await response.json();
